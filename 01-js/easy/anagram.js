a65c8f1bb86d1179b9ca8b5a49180da67a6457a6
Priya-java123 <128849666+Priya-java123@users.noreply.github.com>
2023-06-14 21:37:07 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,27 @@
 */
 
 function isAnagram(str1, str2) {
+ console.log("string1 : " + str1);
+  console.log("string2 : " + str2);
 
+  //converting string to aarray using split() ,then sort(), then convert array to string using join()
+  let sortedStr1 = str1.split("").sort().join("").trim();
+  let sortedStr2 = str2.split("").sort().join("").trim();
+  console.log("sorted array1 : " + sortedStr1);
+  console.log("sorted array2 : " + sortedStr2);
+  //converting String to array
+  let arr1 = sortedStr1.split(" ");
+  let arr2 = sortedStr2.split(" ");
+  //array length
+  if (arr1.length === 0 || arr2.length === 0) {
+    return false;
+  } else {
+    if (sortedStr1 === sortedStr2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = isAnagram;
